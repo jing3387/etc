4b19bb89d00583ed0820a6c878426efc543d6f7a
Jarrod Jeffrey Ingram <jarrod.ingram@rungopher.com>
2018-06-01 09:12:52 +1000

    'vimrc' and 'bash_profile'

--- link.sh
+++ link.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-# Get the path of the directory in which the script is loacted.
 SOURCE=${BASH_SOURCE[0]}
 
-# Resolve $SOURCE until the file is no longer a symlink.
 while [[ -h $SOURCE ]]; do
 	DIR=$(cd -P $(dirname $SOURCE) && pwd)
 	SOURCE=$(readlink $SOURCE)
-	# If $SOURCE was a relative symlink we need to resolve it relative to
-	# the path where the symlink file was located.
 	[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE
 done
 
@@ -18,3 +14,6 @@ for i in $(ls $DIR/dot); do
 	echo Linking $DIR/dot/$i to $HOME/.$i
 	ln -sf $DIR/dot/$i "$HOME/.$i"
 done
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
